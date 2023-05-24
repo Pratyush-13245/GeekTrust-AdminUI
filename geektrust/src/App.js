6b1d36a7ea814660bb3d9b20b881cab44929fa12
Pratyush Tripathi <70906739+Pratyush-13245@users.noreply.github.com>
2023-05-24 23:30:21 +0530

    Add files via upload

---
+++ geektrust/src/App.js
@@ -0,0 +1,12 @@
+import Dashboard from "./component/userdashboard";
+import "./styles.css";
+
+function App() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
